a438e5ea663d59b02a1e9ec8a5c813f98c5a78b9
Nadim-Mahmud <mahmudnadim16@gmail.com>
2021-10-28 00:03:58 +0600

    gray level decreease completed

--- DIP_1_b_decrease_intensity_resolution.m
+++ DIP_1_b_decrease_intensity_resolution.m
@@ -11,19 +11,26 @@ img = imresize(img, [512 512]);
 grayLevels = 8;
 plot_dim = ceil(sqrt(grayLevels));
 
-tmp_img = img;
 
-for i = 1 : grayLevels
-    
-    subplot(plot_dim, plot_dim, i);
-    imshow(tmp_img);
-    title(strcat(int2str(grayLevels-i+1)));
+subplot(plot_dim, plot_dim, 1);
+imshow(im2double(img));
+title(strcat('Gray level :',int2str(grayLevels)));
+
+tmp_img = double(zeros([row column]));
+
+for i = 1 : grayLevels-1
     tmp = 0;
     for j = 1 : row
         for k = 1 : column
-            tmp_img(j,k) = floor(img(j,k)/(2^i));
+            tmp = floor(img(j,k)/(2^i));
+            %dividing by highest value to keep number less than 1
+            tmp_img(j,k) = tmp/(2^grayLevels/2^i);
         end
     end
+    subplot(plot_dim, plot_dim, i+1);
+    %displaying the double image
+    imshow(tmp_img);
+    title(strcat('Gray level :',int2str(grayLevels-i)));
 end
 
 
