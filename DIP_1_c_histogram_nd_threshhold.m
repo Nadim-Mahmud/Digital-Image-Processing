7f8c7d95dd47f72d4ddf486ca0742e3625b17550
Nadim-Mahmud <mahmudnadim16@gmail.com>
2021-10-27 23:37:15 +0600

    showing histogram then thresholding

---
+++ DIP_1_c_histogram_nd_threshhold.m
@@ -0,0 +1,32 @@
+clc;
+close all;
+clear all;
+
+img = imread('dog.jpg');
+img = rgb2gray(img);
+img = imresize(img, [512 512]);
+
+[rows, columns] = size(img);
+
+% decide the threshold value
+imhist(img);
+
+
+%threshold operation
+
+threshold = 75;
+
+output_img = img;
+
+for i = 1 : rows
+    for j = 1 : columns
+        if img(i,j) >= threshold
+            output_img(i,j) = 255;
+        else
+            output_img(i,j) = 0;
+        end
+    end
+end
+
+%imhist(output_img)
+imshow(output_img);
