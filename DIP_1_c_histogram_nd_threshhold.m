dc7817592633bd206c1fb29d08c9a67f0686ede0
Nadim-Mahmud <mahmudnadim16@gmail.com>
2021-10-30 16:44:14 +0600

    thresholding using histogram completed

--- DIP_1_c_histogram_nd_threshhold.m
+++ DIP_1_c_histogram_nd_threshhold.m
@@ -2,31 +2,25 @@ clc;
 close all;
 clear all;
 
-img = imread('dog.jpg');
+img = imread('assets/dog.jpg');
 img = rgb2gray(img);
 img = imresize(img, [512 512]);
 
-[rows, columns] = size(img);
+subplot(3,1,1);
+imshow(img);
+title('Original image');
 
-% decide the threshold value
+% decide the threshold value from the histogram
+subplot(3,1,2);
 imhist(img);
+title('Origian image''s Histogram');
+
+%threshold input
+threshold = input('Enter the threshold value =  ') %200;
+
+output_img = img>threshold;
 
 
-%threshold operation
-
-threshold = 200;
-
-output_img = img;
-
-for i = 1 : rows
-    for j = 1 : columns
-        if img(i,j) >= threshold
-            output_img(i,j) = 255;
-        else
-            output_img(i,j) = 0;
-        end
-    end
-end
-
-%imhist(output_img)
+subplot(3,1,3);
 imshow(output_img);
+title('Threshold');
